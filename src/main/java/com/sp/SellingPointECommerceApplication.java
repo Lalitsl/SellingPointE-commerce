d1276e7fbd20859a108196402947e9676fdf6ceb
Lalitsl <lalitnarware2001@gmail.com>
2024-01-02 18:31:21 +0530

    initial commit

---
+++ src/main/java/com/sp/SellingPointECommerceApplication.java
@@ -0,0 +1,14 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingPointECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingPointECommerceApplication.class, args);
+		System.out.println("selling point e-commerce project started.....");
+	}
+
+}
